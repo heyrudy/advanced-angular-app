7baf2a28fa577f857a6b47dfcee9300cab8454d1
Kokou KOUMAGNA <kokou.koumagnagottoh@gmail.com>
2022-11-27 11:12:49 +0100

    :white_check_mark: Cypress integration in the angular configuration to enable end-2-end, component based tests and fix Jest (unit-test library) module's terms that clashes with the Cypress module's terms.

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,14 +1,15 @@
-import 'webpack.config.js';
 import { defineConfig } from 'cypress';
 
 export default defineConfig({
   e2e: {
     baseUrl: 'http://localhost:3200',
   },
+
   component: {
     devServer: {
-      framework: 'create-react-app',
+      framework: 'angular',
       bundler: 'webpack',
     },
+    specPattern: '**/*.cy.ts',
   },
 });
